60397eb8be9b2b984448c8cbd54da1541fbd7d6c
Musaev Ruslan <musya.wow@gmail.com>
2017-11-13 08:01:03 +0300

    slice

---
+++ trunk/wp-content/themes/fastpost/page.php
@@ -0,0 +1,40 @@
+<?php get_header(); ?>
+
+  <div class="content-wrapper">
+    <div class="container">
+      <div class="left-part">
+        <div class="post-wrapper">
+          <?php /* The loop */ ?>
+          <?php while (have_posts()) : the_post(); ?>
+          <article class="post">
+            <header>
+              <h1><?php the_title(); ?></h1>
+              <span class="date"><?php echo get_the_date(); ?></span>
+              <ul class="tags">
+                <?php the_tags('<li>','</li><li>','</li>'); ?>
+              </ul>
+            </header>
+          <div class="body-article">
+            <?php the_content(); ?>
+            </div>
+            <?php get_template_part('content', get_post_format()); ?>
+
+            <div class="footer-article">
+              <span class="views">4523  Просмотров</span>
+              <ul class="social-elem">
+                <li><a href="#"><img src="theme/images/tmp/soc-1.jpg" alt=""/></a></li>
+                <li><a href="#"><img src="theme/images/tmp/soc-2.jpg" alt=""/></a></li>
+                <li><a href="#"><img src="theme/images/tmp/soc-3.jpg" alt=""/></a></li>
+                <li><a href="#"><img src="theme/images/tmp/soc-4.jpg" alt=""/></a></li>
+              </ul>
+            </div>
+            </article>
+          <?php endwhile; ?>
+        </div>
+      </div>
+      <div class="right-part">
+        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/theme/images/tmp/ads.png" alt=""/>
+      </div>
+    </div>
+  </div>
+<?php get_footer(); ?>
--- trunk/wp-content/themes/fastpost/single.php
+++ trunk/wp-content/themes/fastpost/single.php
@@ -4,34 +4,21 @@
     <div class="container">
       <div class="left-part">
         <div class="post-wrapper">
-
-          <?php
-          $id = 1; // ID заданной рубрики
-          $n = 3;   // количество выводимых записей
-          $posts = query_posts("cat=$id&showposts=$n");
-          if (have_posts()) :  while (have_posts()) : the_post(); ?>
-
+          <?php /* The loop */ ?>
+          <?php while (have_posts()) : the_post(); ?>
             <article class="post">
               <header>
-                <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <h1><?php the_title(); ?></h1>
                 <span class="date"><?php echo get_the_date(); ?></span>
                 <ul class="tags">
                   <?php the_tags('<li>','</li><li>','</li>'); ?>
                 </ul>
               </header>
               <div class="body-article">
-                <div class="img">
-                  <a href="<?php the_permalink(); ?>">
-                    <?php
-                    if (function_exists('add_theme_support'))
-                      the_post_thumbnail(array(600, 9999), array('class' => 'img-polaroid'));
-                    ?></a>
-                </div>
-                <p>
-                  <?php the_excerpt(); ?>
-                </p>
-
+                <?php the_content(); ?>
               </div>
+              <?php get_template_part('content', get_post_format()); ?>
+
               <div class="footer-article">
                 <span class="views"><?php if(function_exists('the_views')) { the_views(); } ?>  Просмотров</span>
                 <ul class="social-elem">
@@ -42,12 +29,7 @@
                 </ul>
               </div>
             </article>
-
-          <?php endwhile;
-          else: ?>
-            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-          <?php endif; ?>
-
+          <?php endwhile; ?>
         </div>
       </div>
       <div class="right-part">
