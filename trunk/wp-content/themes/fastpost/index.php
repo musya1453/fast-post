aab4bf6b208ac800986fb345a68ca13eabbf4688
Musaev Ruslan <musya.wow@gmail.com>
2017-11-15 10:13:57 +0300

    slice

--- trunk/wp-content/themes/fastpost/index.php
+++ trunk/wp-content/themes/fastpost/index.php
@@ -16,7 +16,7 @@
                 <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
                 <span class="date"><?php echo get_the_date(); ?></span>
                 <ul class="tags">
-                  <?php the_tags('<li>','</li><li>','</li>'); ?>
+                  <?php the_tags('<li>', '</li><li>', '</li>'); ?>
                 </ul>
               </header>
               <div class="body-article">
@@ -27,15 +27,17 @@
                       the_post_thumbnail(array(600, 9999), array('class' => 'img-polaroid'));
                     ?></a>
                 </div>
-               <div class="text">
-                 <p>
-                   <?php the_excerpt(); ?>
-                 </p>
-               </div>
+                <div class="text">
+                  <p>
+                    <?php the_excerpt(); ?>
+                  </p>
+                </div>
 
               </div>
               <div class="footer-article">
-                <span class="views"><?php if(function_exists('the_views')) { the_views(); } ?>  Просмотров</span>
+                <span class="views"><?php if (function_exists('the_views')) {
+                    the_views();
+                  } ?>  Просмотров</span>
                 <ul class="social-elem">
                   <li><a href="#"><img src="theme/images/tmp/soc-1.jpg" alt=""/></a></li>
                   <li><a href="#"><img src="theme/images/tmp/soc-2.jpg" alt=""/></a></li>
@@ -44,12 +46,10 @@
                 </ul>
               </div>
             </article>
-
           <?php endwhile;
           else: ?>
             <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
           <?php endif; ?>
-
         </div>
       </div>
       <div class="right-part">
